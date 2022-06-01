cbcd619b61cdfd3bd0b2b3ef468f26ce98ccaffe
0jiiino <tnqkrtnqkr7756@gmail.com>
2022-06-02 04:36:13 +0900

    feat: Add styled-jsx logic

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,27 @@
-const Card = ({ number, title, created_at, comments }) => {
+import css from "styled-jsx/css";
+
+const style = css`
+  .card {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    margin: 10px;
+    width: 300px;
+    height: 220px;
+    padding: 20px;
+    border: 1px solid #f2f2f2;
+    border-radius: 2px;
+  }
+`;
+
+const Card = ({ number, title, children }) => {
   return (
-    <div>
-      <div>title : {title}</div>
-      <div>issueNumber : {number}</div>
-      <div>created_at : {created_at}</div>
-      <div>comments : {comments}</div>
+    <div className="card">
+      <h2 className="header-sm center-text">#{number}</h2>
+      <h2 className="header-sm center-text">{title}</h2>
+      {children}
+      <style jsx>{style}</style>
     </div>
   );
 };
--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,15 +1,54 @@
+import { format } from "date-fns";
+import { FaComments, FaCalendarAlt } from "react-icons/fa";
+import css from "styled-jsx/css";
+
 import Card from "./Card";
 
+const style = css`
+  .card-list {
+    margin: 10px;
+  }
+  .card-list li {
+    display: flex;
+    justify-content: flex-start;
+    font-weight: 300;
+    font-size: 16px;
+  }
+  .card-list li:not(:last-of-type) {
+    margin-bottom: 5px;
+  }
+  .card-list svg {
+    margin-right: 5px;
+    height: 20px;
+  }
+`;
+
 const CardList = ({ dataList }) => {
-  return dataList.map(data => (
-    <Card
-      key={data.number}
-      number={data.number}
-      title={data.title}
-      created_at={data.created_at}
-      comments={data.comments}
-    />
-  ));
+  return (
+    <ul className="flex space-between">
+      {dataList.map(data => {
+        const date = format(new Date(data.created_at), "yyyy-MM-dd HH:mm:ss");
+
+        return (
+          <li key={data.node_id}>
+            <Card key={data.number} number={data.number} title={data.title}>
+              <ul className="card-list">
+                <li>
+                  <FaComments color="rgb(255, 215, 0)" size={14} />
+                  {data.comments}
+                </li>
+                <li>
+                  <FaCalendarAlt color="rgb(129, 195, 245)" size={14} />
+                  {date}
+                </li>
+              </ul>
+            </Card>
+          </li>
+        );
+      })}
+      <style jsx>{style}</style>
+    </ul>
+  );
 };
 
 export default CardList;
